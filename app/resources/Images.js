7ae76a7a9048dee141a429872d5438bedcd9e01b
Zoey <807861340+Urwateryi@users.noreply.github.com>
2018-04-20 17:35:32 +0800

    增加图片管理类

---
+++ app/resources/Images.js
@@ -0,0 +1,35 @@
+/**
+ * Description:图片管理类
+ * 为了区分图片，此处按照不同的功能板块将图片分类
+ */
+export default {
+
+    home:{
+        ic_more:require('./imgs/ic_more.png'),
+    },
+
+    menu:{
+        ic_menu_setting:require('./imgs/ic_menu_setting.png'),
+        ic_menu_night_state:require('./imgs/ic_menu_night_state.png'),
+        ic_menu_offline:require('./imgs/ic_menu_offline.png'),
+        ic_menu_recomend:require('./imgs/ic_menu_recomend.png'),
+
+        ic_menu_about:require('./imgs/ic_menu_about.png'),
+        ic_menu_category:require('./imgs/ic_menu_category.png'),
+        ic_menu_column:require('./imgs/ic_menu_column.png'),
+        ic_menu_msg:require('./imgs/ic_menu_msg.png'),
+        ic_menu_usercenter:require('./imgs/ic_menu_usercenter.png'),
+        ic_menu_home:require('./imgs/ic_menu_home.png'),
+
+        ic_menu_close_x:require('./imgs/ic_menu_close_x.png'),
+    },
+
+    item:{
+        ic_comment_normal:require('./imgs/ic_comment_normal.png'),
+        ic_comment_press:require('./imgs/ic_comment_press.png'),
+        ic_share_normal:require('./imgs/ic_share_normal.png'),
+        ic_share_press:require('./imgs/ic_share_press.png'),
+        ic_favor_normal:require('./imgs/ic_favor_normal.png'),
+        ic_favor_press:require('./imgs/ic_favor_press.png'),
+    }
+}
